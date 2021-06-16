d0ff55c9a5a8ed7d751a497502f2d50694e969e7
DESKTOP-U3578U6\rst <1175215747@qq.com>
2021-06-16 14:00:49 +0800

    1

--- smart-cms-base-common/src/main/java/com/smart/cms/utils/other/CommonCst.java
+++ smart-cms-base-common/src/main/java/com/smart/cms/utils/other/CommonCst.java
@@ -1,47 +1,32 @@
 package com.smart.cms.utils.other;
 
-/**
- * 公共静态变量
- *
- * @author liuyongtao
- * @create 2017-11-21 17:38
- */
+
 public class CommonCst {
 
     /**
      * 上传文件的类型-markdown
      *
-     * @author LiuYongTao
-     * @date 2019/8/8 10:15
      */
     public static final String UPLOAD_TYPE_MARKDOWN = "markdown";
     /**
      * 上传文件的类型-userIcon
      *
-     * @author LiuYongTao
-     * @date 2019/8/8 10:15
      */
     public static final String UPLOAD_TYPE_USER_ICON = "userIcon";
 
     /**
      * 审核状态，0-提交
      *
-     * @author billow
-     * @date 2019/9/8 19:49
      */
     public static final String APPROVE_STATUS_SUBMIT = "0";
     /**
      * 审核状态：1-退回后提交
      *
-     * @author billow
-     * @date 2019/9/8 19:50
      */
     public static final String APPROVE_STATUS_resubmit = "1";
     /**
      * 审核状态：2-退回
      *
-     * @author billow
-     * @date 2019/9/8 19:50
      */
     public static final String APPROVE_STATUS_REWORK = "2";
 
