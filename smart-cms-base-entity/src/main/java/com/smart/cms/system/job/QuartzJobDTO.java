e297da6b18501907152062353a48434c06f88a2a
DESKTOP-U3578U6\rst <1175215747@qq.com>
2021-06-25 18:26:24 +0800

    job 没弄完

--- smart-cms-base-entity/src/main/java/com/smart/cms/system/job/QuartzJobDTO.java
+++ smart-cms-base-entity/src/main/java/com/smart/cms/system/job/QuartzJobDTO.java
@@ -3,6 +3,7 @@ package com.smart.cms.system.job;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.smart.cms.base.BaseEntityData;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -19,5 +20,28 @@ import java.io.Serializable;
 @ApiModel(value = "定时任务", description = "定时任务")
 public class QuartzJobDTO extends BaseEntityData implements Serializable {
 
+    public static final String JOB_KEY = "JOB_KEY";
+
     private static final long serialVersionUID = 2944601784762634657L;
+
+    @ApiModelProperty(value = "定时器名称")
+    private String jobName;
+
+    @ApiModelProperty(value = "Bean名称")
+    private String beanName;
+
+    @ApiModelProperty(value = "方法名称")
+    private String methodName;
+
+    @ApiModelProperty(value = "参数")
+    private String params;
+
+    @ApiModelProperty(value = "cron表达式")
+    private String cronExpression;
+
+    @ApiModelProperty(value = "状态，0暂时或1启动")
+    private int isPause = 0;
+
+    @ApiModelProperty(value = "备注")
+    private String description;
 }
